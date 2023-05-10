88a9584b7e5dcccb0bf2c084f8717fed10666857
Imani 'K!ΠG' Niyigena <imanix8@gmail.com>
2023-05-10 07:38:19 +0200

    Evershop Client side configuration

---
+++ .evershop/build/frontStore/paypalReturn/server/entry.jsx
@@ -0,0 +1,60 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import Area from '@evershop/evershop/src/components/common/Area';
+Area.defaultProps.components = {
+  'icon-wrapper': {
+    XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY2hlY2tvdXRccGFnZXNcZnJvbnRTdG9yZVxhbGxcTWluaUNhcnQuanN4: {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY2hlY2tvdXRccGFnZXNcZnJvbnRTdG9yZVxhbGxcTWluaUNhcnQuanN4',
+      sortOrder: 10,
+      component: require('C:\\Users\\imani\\OneDrive\\Desktop\\GitHub Projects\\ImaniAN\\Iduka\\node_modules\\@evershop\\evershop\\src\\modules\\checkout\\pages\\frontStore\\all\\MiniCart.jsx')
+    },
+    XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE1vYmlsZU1lbnUuanN4: {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE1vYmlsZU1lbnUuanN4',
+      sortOrder: 50,
+      component: require('C:\\Users\\imani\\OneDrive\\Desktop\\GitHub Projects\\ImaniAN\\Iduka\\node_modules\\@evershop\\evershop\\src\\modules\\cms\\pages\\frontStore\\all\\MobileMenu.jsx')
+    },
+    XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY3VzdG9tZXJccGFnZXNcZnJvbnRTdG9yZVxhbGxcVXNlckljb24uanN4: {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY3VzdG9tZXJccGFnZXNcZnJvbnRTdG9yZVxhbGxcVXNlckljb24uanN4',
+      sortOrder: 30,
+      component: require('C:\\Users\\imani\\OneDrive\\Desktop\\GitHub Projects\\ImaniAN\\Iduka\\node_modules\\@evershop\\evershop\\src\\modules\\customer\\pages\\frontStore\\all\\UserIcon.jsx')
+    }
+  },
+  body: {
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXExheW91dC5qc3g=': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXExheW91dC5qc3g=',
+      sortOrder: 1,
+      component: require('C:\\Users\\imani\\OneDrive\\Desktop\\GitHub Projects\\ImaniAN\\Iduka\\node_modules\\@evershop\\evershop\\src\\modules\\cms\\pages\\frontStore\\all\\Layout.jsx')
+    },
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE5vdGlmaWNhdGlvbi5qc3g=': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE5vdGlmaWNhdGlvbi5qc3g=',
+      sortOrder: 10,
+      component: require('C:\\Users\\imani\\OneDrive\\Desktop\\GitHub Projects\\ImaniAN\\Iduka\\node_modules\\@evershop\\evershop\\src\\modules\\cms\\pages\\frontStore\\all\\Notification.jsx')
+    }
+  },
+  header: {
+    XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXExvZ28uanN4: {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXExvZ28uanN4',
+      sortOrder: 1,
+      component: require('C:\\Users\\imani\\OneDrive\\Desktop\\GitHub Projects\\ImaniAN\\Iduka\\node_modules\\@evershop\\evershop\\src\\modules\\cms\\pages\\frontStore\\all\\Logo.jsx')
+    },
+    XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE1lbnUuanN4: {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE1lbnUuanN4',
+      sortOrder: 5,
+      component: require('C:\\Users\\imani\\OneDrive\\Desktop\\GitHub Projects\\ImaniAN\\Iduka\\node_modules\\@evershop\\evershop\\src\\modules\\cms\\pages\\frontStore\\all\\Menu.jsx')
+    }
+  },
+  head: {
+    XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE1ldGEuanN4: {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNcY21zXHBhZ2VzXGZyb250U3RvcmVcYWxsXE1ldGEuanN4',
+      sortOrder: 5,
+      component: require('C:\\Users\\imani\\OneDrive\\Desktop\\GitHub Projects\\ImaniAN\\Iduka\\node_modules\\@evershop\\evershop\\src\\modules\\cms\\pages\\frontStore\\all\\Meta.jsx')
+    }
+  },
+  content: {
+    'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNccGF5cGFsXHBhZ2VzXGZyb250U3RvcmVccGF5cGFsUmV0dXJuXEVycm9yLmpzeA==': {
+      id: 'XG5vZGVfbW9kdWxlc1xAZXZlcnNob3BcZXZlcnNob3Bcc3JjXG1vZHVsZXNccGF5cGFsXHBhZ2VzXGZyb250U3RvcmVccGF5cGFsUmV0dXJuXEVycm9yLmpzeA==',
+      sortOrder: 10,
+      component: require('C:\\Users\\imani\\OneDrive\\Desktop\\GitHub Projects\\ImaniAN\\Iduka\\node_modules\\@evershop\\evershop\\src\\modules\\paypal\\pages\\frontStore\\paypalReturn\\Error.jsx')
+    }
+  }
+} 
